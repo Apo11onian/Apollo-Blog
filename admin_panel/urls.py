48f685addc2a5b85fb48fa99739c2b1e7ef1ac78
Apo11onian <Apo11onian@yandex.ru>
2017-10-21 22:32:53 +0330

    Add url for Logout view to urls.py

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path('logout', views.LogoutView.as_view(), name="Logout"),
     path('login/', views.LoginView.as_view(), name="LoginPage"),
     path('panel', views.Panel.as_view(), name="Panel"),
     path('', RedirectView.as_view(url="login"), name="LoginPage"),
