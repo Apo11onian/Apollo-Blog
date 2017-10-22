a0fdd5a1ed8bbc70122e4b00beca67bf244cccc1
Mahdi Zareie <Mahdi.elf@gmail.com>
2017-10-22 22:57:48 +0330

    Override save() method to fill require fields before saving
    Warning: It's still not working since there is no next url

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from blog_posting import models
+from datetime import datetime
 
 
 class CreatePost(forms.ModelForm):
@@ -7,5 +8,12 @@ class CreatePost(forms.ModelForm):
         model = models.Post
         exclude = ("created_at", "modified_at", "published")
 
-
+    def save(self, commit=True):
+        blog_post_object = super(CreatePost, self).save(commit=False)
+        assert isinstance(blog_post_object, models.Post)
+        blog_post_object.published = False
+        blog_post_object.modified_at = datetime.now()
+        blog_post_object.created_at = datetime.now()
+        blog_post_object.save()
+        return blog_post_object
 
