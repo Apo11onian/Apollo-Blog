1104418598f58a39e33d7cb0abfc8c1d0e7b49e9
Mahdi Zareie <Mahdi.elf@gmail.com>
2017-10-27 21:44:11 +0330

    Make text a field in Post model

--- blog_posting/models.py
+++ blog_posting/models.py
@@ -8,30 +8,31 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=280)
     abstract = models.TextField()
+    text = models.TextField()
     published = models.BooleanField(default=False)
     created_at = models.DateTimeField()
     modified_at = models.DateTimeField()
 
-
-class PostContent(models.Model):
-    class Meta:
-        unique_together = (("post", "order",),)
-
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    content_type = models.CharField(max_length=16, choices=[])
-    tags = models.ManyToManyField(Tag)
-    order = models.IntegerField(default=0)
-
-
-class TextContent(PostContent):
-    text = models.TextField()
-
-
-class ImageContent(PostContent):
-    image = models.ImageField()
-    alt = models.CharField(max_length=64)
-
-
-class CodeContent(PostContent):
-    code = models.TextField()
-    language = models.CharField(max_length=32)
+# In order to not to forget about these I keep them here
+# class PostContent(models.Model):
+#     class Meta:
+#         unique_together = (("post", "order",),)
+#
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     content_type = models.CharField(max_length=16, choices=[])
+#     tags = models.ManyToManyField(Tag)
+#     order = models.IntegerField(default=0)
+#
+#
+# class TextContent(PostContent):
+#     text = models.TextField()
+#
+#
+# class ImageContent(PostContent):
+#     image = models.ImageField()
+#     alt = models.CharField(max_length=64)
+#
+#
+# class CodeContent(PostContent):
+#     code = models.TextField()
+#     language = models.CharField(max_length=32)
